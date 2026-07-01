1b7fd67874b88c6267cc1f7413037a87836fe84e
abdumuqeemshaik <abdulmuqeemshaik04@gmail.com>
2026-07-01 21:08:21 +0530

    Time: 7 ms (56.21%), Space: 19.1 MB (98.99%) - LeetHub

--- 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.py
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.py
@@ -3,10 +3,11 @@ class Solution:
         count=0
         for i in range(1,num+1):
             temp=i
-            sum=0
-            while temp>0:
-                sum+=temp%10
-                temp//=10
-            if sum%2==0:
+            Sum=0
+            while(temp>0):
+                digit=temp%10
+                Sum+=digit
+                temp=temp//10
+            if(Sum%2==0):
                 count+=1
         return count
