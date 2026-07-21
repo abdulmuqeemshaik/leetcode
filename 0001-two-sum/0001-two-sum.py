9cc770ca19ee8fbda7de0c053491a67ee9a4b095
abdulmuqeem <abdulmuqeemshaik04@gmail.com>
2026-07-21 21:29:39 +0530

    Time: 1 ms (61.41%), Space: 20.6 MB (6.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n=len(nums)
-        for i in range(n-1):
-            for j in range(i+1,n):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
-        return []  # No solution found
+        # n=len(nums)
+        # for i in range(n-1):
+        #     for j in range(i+1,n):
+        #         if nums[i]+nums[j]==target:
+        #             return [i,j]
+        # return []  # No solution found
+        num_to_index={} 
+        for i, num in enumerate(nums):
+            complement=target-num
+            # If complement exists, we found our pair
+            if complement in num_to_index:
+                return [num_to_index[complement],i]
+            # Store current number and its index in the hash map
+            num_to_index[num]=i
