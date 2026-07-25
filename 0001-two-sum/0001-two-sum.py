8edc55f527a0c0aa7288d54f5a6b26bd231e4038
abdulmuqeem <abdulmuqeemshaik04@gmail.com>
2026-07-25 18:57:17 +0530

    Time: 0 ms (100%), Space: 20.4 MB (40.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,28 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        # brute force
         # n=len(nums)
         # for i in range(n-1):
         #     for j in range(i+1,n):
         #         if nums[i]+nums[j]==target:
         #             return [i,j]
         # return []  # No solution found
-        num_to_index={} 
-        for i,num in enumerate(nums):
-            complement=target-num
-            # If complement exists, we found our pair
-            if complement in num_to_index:
-                return [num_to_index[complement],i]
-            # Store current number and its index in the hash map
-            num_to_index[num]=i
+        
+        # optimal  
+        d={}
+        for i in range(len(nums)):
+            diff=target-nums[i]
+            if diff in  d:
+                return (d[diff],i)
+                break
+            d[nums[i]]=i
+        
+        # num_to_index={} 
+        # for i,num in enumerate(nums):
+        #     complement=target-num
+        #     # If complement exists, we found our pair
+        #     if complement in num_to_index:
+        #         return [num_to_index[complement],i]
+        #     # Store current number and its index in the hash map
+        #     num_to_index[num]=i
